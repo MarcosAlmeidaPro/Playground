ab383675a4c2c14f996cdd51aa2d6e040775d3ed
Mark Dev <marcosalmeidapro@gmail.com>
2017-03-10 12:15:39 -0300

    First commit
    Some playground works and tests

---
+++ Fibonacci.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var num1 = 1
+
+var num2 = 1
+
+var num3 = Int()
+
+var arrayFibonacci = [0, 1, 1]
+
+
+func fibonacci(x: Int){
+    
+    for index in 3...x {
+        
+        var novoNumero = arrayFibonacci[index-1] + arrayFibonacci[index-2]
+        
+        arrayFibonacci.append(novoNumero)
+        
+    }
+    
+    
+    
+}
+
+var numeroSelecao = 50
+
+fibonacci(x: numeroSelecao)
+
+
+print(arrayFibonacci[numeroSelecao])
+
+arrayFibonacci.count
+
+print(arrayFibonacci)
+
+
+
+
+
+
+    
---
+++ Remainder.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numUm = 3
+
+var numDois = 0
+
+var resultado: Double!
+
+resultado = Double(numUm) * Double(numDois)
+
+resultado.remainder(dividingBy: Double(Int(resultado)))
+
